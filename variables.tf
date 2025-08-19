8698f0580a38d658fdbc403bcb55763606acb682
TEK Student <TEKstudent@vlab.lan>
2025-08-19 13:59:34 -0700

    Fix integration tests to comply with new variable validations

--- variables.tf
+++ variables.tf
@@ -13,7 +13,6 @@ variable "instance_type" {
     error_message = "All EC2 instances must be t2.micro type"
   }
 }
-
 variable "subnet_ids" {
   description = "Subnet IDs for EC2 instances"
   type        = list(string)
@@ -23,6 +22,7 @@ variable "security_group_ids" {
   description = "Security group IDs for EC2 instances"
   type        = list(string)
 }
+
 
 variable "tags" {
   description = "Tags for the EC2 instances"
