a698f3fd04c1e6192bab341b58d365b369d7e4f5
TEK Student <TEKstudent@vlab.lan>
2025-08-19 14:13:46 -0700

    Fix integration tests to comply with new variable validations

--- variables.tf
+++ variables.tf
@@ -6,6 +6,10 @@ variable "instance_count" {
 variable "instance_type" {
   description = "Type of EC2 instance to use"
   type        = string
+  validation {
+    condition     = var.instance_type == "t2.micro"
+    error_message = "All EC2 instances must be t2.micro type"
+  }
 }
 
 variable "subnet_ids" {
@@ -20,29 +24,13 @@ variable "security_group_ids" {
 
 variable "tags" {
   description = "Tags for instances"
-  type        = map
-  default     = {}
-}
-
-
-
-# variables.tf in terraform-aws-ec2-instance-tests-{your-initials}
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  
-  validation {
-    condition     = var.instance_type == "t2.micro"
-    error_message = "All EC2 instances must be t2.micro type"
-  }
-}
-
-variable "tags" {
-  description = "Tags for the EC2 instances"
   type        = map(string)
-  
+  default     = {}
+
   validation {
     condition     = contains(keys(var.tags), "project")
     error_message = "All EC2 instances must have project tag"
   }
 }
+
+
