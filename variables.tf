4aef201f74aedff05d27dc8ada528567931bc312
TEK Student <TEKstudent@vlab.lan>
2025-08-20 05:45:56 -0700

    Fix integration tests to comply with new variable validations

--- variables.tf
+++ variables.tf
@@ -25,6 +25,8 @@ variable "security_group_ids" {
 variable "tags" {
   description = "Tags for instances"
   type        = map(string)
+  default     = {project     = "project-alpha"}
+
   validation {
     condition     = contains(keys(var.tags), "project")
     error_message = "All EC2 instances must have project tag"
